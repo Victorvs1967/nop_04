ccd02a64a9466c57fb6c26bfd4f44477e1209329
Victor <victorsmirnov@me.com>
2020-05-09 00:32:18 +0300

    aa

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')    куегкт кутвук_еуьздфеу(эфищгеюреьдэ)
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
